0fb09bfd3a8845bf76125c983c7d8e92f1197921
Petr Marek <contact@petrmarek.eu>
2016-03-04 17:54:40 +0100

    smarter toggle wrap

--- subl/Packages/UserCss/toggle_math_wrap.py
+++ subl/Packages/UserCss/toggle_math_wrap.py
@@ -3,10 +3,23 @@ import re
 
 class ToggleMathWrap(sublime_plugin.TextCommand):
     def run(self, edit):
-        line = self.view.line(self.view.sel()[0])
-        line_text = self.view.substr(line)
+        view = self.view
+        sel = view.sel()[0]
+        line = view.line(sel)
+        line_text = view.substr(line)
+        if sel.empty():
+            selection = None
+        else:
+            selection = sel
         if '((' in line_text:
             new_text = re.sub(r'\(\( (.+) \)\)', r'\1', line_text)
+            return view.replace(edit, line, new_text)
         else:
-            new_text = re.sub(r'([^:]+: )(.+)', r'\1(( \2 ))', line_text)
-        self.view.replace(edit, line, new_text)
+            if selection:
+                # wrap the selected text
+                new_text = '(( ' + view.substr(selection) + ' ))'
+                return view.replace(edit, selection, new_text)
+            else:
+                # wrap the part after colon
+                new_text = re.sub(r'([^:]+: )(.+)', r'\1(( \2 ))', line_text)
+                return view.replace(edit, line, new_text)
