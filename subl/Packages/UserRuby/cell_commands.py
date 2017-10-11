1c515e0486533a3fe4ab61b7b87f8bd726a7b08d
Petr Marek <contact@petrmarek.eu>
2017-10-11 11:44:24 +0200

    cell_commands namespaces

--- subl/Packages/UserRuby/cell_commands.py
+++ subl/Packages/UserRuby/cell_commands.py
@@ -3,14 +3,16 @@ import sublime_plugin
 import re
 import subprocess
 
+namespaces = '(?:\w+/)*'
+
 def get_cell_name(window):
     view = window.active_view()
     path = view.file_name()
 
     try:
-        return re.search('app/cells/(\w+)/.+\.slim', path).group(1)
+        return re.search('app/cells/' + namespaces + '(\w+)/.+\.slim', path).group(1)
     except (IndexError, AttributeError):
-        return re.search('app/cells/(\w+)_cell\.rb', path).group(1)
+        return re.search('app/cells/' + namespaces + '(\w+)_cell\.rb', path).group(1)
     except (IndexError, AttributeError):
         return None
 
@@ -37,7 +39,7 @@ class CellOpenAll(sublime_plugin.WindowCommand):
     def run(self):
         cell_name = get_cell_name(self.window)
         pwd = self.window.folders()[0]
-        files = subprocess.check_output('pt --follow -g="app/cells/' + cell_name + '(_cell\.rb|/.+)" ' + pwd, shell = True).decode('utf-8').split('\n')
+        files = subprocess.check_output('pt --follow -g="app/cells/' + namespaces + cell_name + '(_cell\.rb|/.+)" ' + pwd, shell = True).decode('utf-8').split('\n')
         for file in files:
             if file:
                 self.window.open_file(file)
@@ -57,7 +59,7 @@ class CellOpen(sublime_plugin.WindowCommand):
         else:
             return self.window.status_message('Not a valid target.')
 
-        pattern = 'app/cells/' + pattern
+        pattern = 'app/cells/' + namespaces + pattern
         pwd = self.window.folders()[0]
         files = subprocess.check_output('pt --follow -g="' + pattern + '" ' + pwd, shell = True).decode('utf-8').split('\n')
         files.remove('')
