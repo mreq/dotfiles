452422295ec764b817a4d802b910e6777de2d727
Petr Marek <contact@petrmarek.eu>
2024-07-05 06:42:18 +0200

    mv ssh-askpass

---
+++ ssh-askpass/sway-ssh-askpass.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+alacritty --class sway-ssh-askpass --config-file ~/.dotfiles/ssh-askpass/alacritty-sway-ssh-askpass.toml
--- sway/sway-ssh-askpass.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-alacritty --class sway-ssh-askpass --config-file ~/.dotfiles/alacritty/alacritty-sway-ssh-askpass.toml
