40ae93796f5645fbc42536d01183f658f5b4ecd4
Petr Marek <contact@petrmarek.eu>
2024-07-14 16:39:54 +0200

    reformat using black

--- python/modules/dotfiles/rofi.py
+++ python/modules/dotfiles/rofi.py
@@ -2,12 +2,11 @@
 
 import subprocess
 
-def dmenu(list, color = "blue"):
-    echo_part = "echo \"" + "\n".join(list) + "\""
+
+def dmenu(list, color="blue"):
+    echo_part = 'echo "' + "\n".join(list) + '"'
     rofi_part = "rofi -dmenu -theme base16-mreq-" + color
     command = echo_part + " | " + rofi_part
-
-    print(command)
 
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     return result.stdout.strip() or None
